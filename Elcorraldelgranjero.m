bf576c522bec73708dc9e8f7a6c20c242b6c4677
FernandoJesusParedesMtz17 <90364947+FernandoJesusParedesMtz17@users.noreply.github.com>
2021-10-14 23:45:48 -0500

    Primer entregable del trabajo en equipo
    El trabajo a entregar de dicho equipo ya antes mencionados los integrantes.

---
+++ Elcorraldelgranjero.m
@@ -0,0 +1,29 @@
+%Octave.script  
+% Title               :El corral del granero
+% Description         :Encontrar la dimension del corral a construir
+% Author              :Fernando Jesús Paredes Martínez,
+% Date                :20213010,
+% Version             :1
+% Usage               :octave> /path/Actividad/Elcorraldelgranjero
+
+clear
+
+%Dimensiones del corral con área máxima 
+pkg load symbolic
+syms x y
+
+%Rango de la base y de la altura 
+x=[0 : 1 : 60];
+y=[0 : 1 : 40];
+
+%Función a plotear para obtener el área máxima  
+a=(480 * x / 3 - 8 * x.^2 / 3)
+
+%Funcion a plotear
+plot(a)
+
+%Dibujar 
+plot(a)
+
+%Titulo
+title(['El corral del granjero']);
